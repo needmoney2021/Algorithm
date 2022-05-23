d6936ac665c6f63d94be0407fa4871e0fba75639
needmoney <limemimorinn@gmail.com>
2022-05-23 15:06:52 +0900

    improve

--- TwoPointer/subtotal.py
+++ TwoPointer/subtotal.py
@@ -42,6 +42,37 @@ def get_subtotal(source: List[int], target: int) -> List[List[int]]:
     return result
 
 
+def get_subtotal_v2(source: List[int], target: int) -> List[List[int]]:
+    # end가 배열의 끝일 때 subtotal >= target이면 start를 더 줄일 여지가 있다.
+    # subtotal < target이면 더 이상 탐색해도 의미가 없다. 더 증가할 여지는 없고 빠지기만 하는데 이미 작으니까.
+    # start, end 증가 조건이 end가 배열 끝일 때 탐색을 더 할 지 말지를 정하는 조건과 동일하므로 같은 분기에서 비교한다.
+
+    result: List[List[int]] = []
+    start = end = 0
+    subtotal = 0
+
+    while True:
+        # start, end는 배열의 길이를 초과할 수 없다.
+        # 따라서 먼저 start를 올리는 조건을 체크한 후
+        if subtotal >= target:
+            subtotal -= source[start]
+            start += 1
+
+        # end가 배열의 끝인지를 확인한다.
+        elif end == len(source):
+            break
+
+        # 마지막으로 end를 증가시키는 경우를 체크한다.
+        elif subtotal < target:
+            subtotal += source[end]
+            end += 1
+
+        if subtotal == target:
+            result.append(source[start:end])
+
+    return result
+
+
 if __name__ == "__main__":
 
     source1 = [i for i in range(1, 6)]
@@ -58,3 +89,15 @@ if __name__ == "__main__":
     result3 = get_subtotal(source3, 11)
     print(result3)
     # [[2, 9], [11]]
+
+    source4 = [i for i in range(1, 6)]
+    result4 = get_subtotal_v2(source4, 5)
+    print(result4)
+
+    source5 = [i for i in range(1, 11)]
+    result5 = get_subtotal_v2(source5, 22)
+    print(result5)
+
+    source6 = [5, 3, 7, 2, 9, 11, 4, 6, 2]
+    result6 = get_subtotal_v2(source6, 11)
+    print(result6)
